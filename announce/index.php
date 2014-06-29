ca1a006f6f01da41f4bf84861f34a4b9b21b55ae
alex <acritsc1@jhu.edu>
2014-06-29 15:43:10 -0400

    Adding changes for week #6

---
+++ announce/index.php
@@ -0,0 +1,143 @@
+<?php
+  $root_path = "../";
+  include_once('../common.php');
+?>
+
+<html>
+	<head>
+		<title>
+			Cancer Variant Database: Announcements
+		</title>
+		<link href="<?php echo $root_path;?>bootstrap.css" rel="stylesheet">
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" ></script>
+    <script src="<?php echo $root_path;?>site.js" ></script>
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		<script src="http://cytoscape.github.io/cytoscape.js/api/cytoscape.js-latest/cytoscape.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo $root_path;?>styles.css">
+	</head>
+
+	<body style="background:#fafafa;">
+	<div class="jumbotron" style="margin-bottom:0px;height:100%">
+	  <div class="container" style="margin-bottom:15px;">
+	    <h1><a href="<?php echo $root_path;?>"><span style="color:#ea2f10">Can-VD</span>: The <span style="color:#ea2f10">Can</span>cer <span style="color:#ea2f10">V</span>ariant <span style="color:#ea2f10">D</span>atabase</a></h1>
+	    <p class="pull-right" style="margin-left:10px;margin-top:5px;"><a class="btn btn-danger" href="<?php echo $root_path;?>about" role="button"><i class="fa fa-flask"></i> About </a>
+	    <a class="btn btn-default" href="<?php echo $root_path;?>faqs" role="button"><i class="fa fa-question"></i> FAQs</a>
+	    <a class="btn btn-default" href="<?php echo $root_path;?>contact" role="button"><i class="fa fa-envelope-o"></i> Contact</a>
+	    </p>
+	    <p style="margin-bottom:50px;margin-top:30px;" class="lead"><b>Can-VD: News and Announcements</b></p>
+
+      <?php
+                             $query = 'SELECT *
+                                      FROM announcements WHERE `show`=1 ORDER BY id DESC;';
+                             $query_params = array();
+                             $stmt = $dbh->prepare($query);
+                             $stmt->execute($query_params);
+
+                              while ($row = $stmt->fetch())
+                              {
+
+                                ?>
+        <div style="margin-top:25px;margin-left:40px;">
+        <h4  style="display:block;margin-bottom:4px;"><a href="#" style="display:block;padding-bottom:4px;margin-bottom:3px;"><?php echo $row[2];?></a><i style="padding-right:5px;font-size:0.75em;"><?php echo $row[1];?> </i></h4>
+        <p><?php echo $row[3];?></p>
+        </div>
+        <?php
+      }
+      ?>
+	  </div>
+	<div class="container">
+	
+  <?php
+      include $root_path. 'footer.php';
+    ?>
+
+	</div>
+
+	</div>
+
+	<script>
+	$(loadCy = function(){
+
+  options = {
+  	name: 'circle',
+    showOverlay: true,
+    minZoom: 0.5,
+    maxZoom: 2,
+    fit:true,
+
+    style: cytoscape.stylesheet()
+      .selector('node')
+        .css({
+          'background-color': '#c4c4c4',
+          'content': 'data(name)',
+          'font-family': 'helvetica',
+          'font-size': 14,
+          'text-valign': 'center',
+          'color': '#0b1a1e',
+          'width': 'mapData(weight, 30, 80, 20, 50)',
+          'height': 'mapData(height, 0, 200, 10, 45)',
+          'border-color': '#fff'
+        })
+      .selector(':selected')
+        .css({
+          'background-color': '#f04124',
+          'line-color': '#000',
+          'target-arrow-color': '#000',
+          'text-outline-color': '#000'
+        })
+      .selector('edge')
+        .css({
+          'width': 2,
+          'line-color': 'data(func)',
+          'line-style': 'data(type)',
+          'target-arrow-shape': 'triangle'
+        })
+    ,
+
+    elements: {
+      nodes: [
+        {
+          data: { id: 'j', name: 'ProtJ', weight: 65, height: 150 }
+        },
+
+        {
+          data: { id: 'e', name: 'ProtE', weight: 65, height: 150 }
+        },
+
+        {
+          data: { id: 'k', name: 'ProtK', weight: 65, height: 150 }
+        },
+
+        {
+          data: { id: 'g', name: 'ProtG', weight: 65, height: 150 }
+        }
+      ],
+
+      edges: [
+        { data: { source: 'j', target: 'e', type: 'dashed', func:'#388f58' } },
+        { data: { source: 'j', target: 'k', type: 'dashed', func:'#388f58' } },
+        { data: { source: 'j', target: 'g', func:'#388f58', func: '#dc2c0f' }  },
+
+        { data: { source: 'e', target: 'j', func:'#39b3d7' }  },
+        { data: { source: 'e', target: 'k', type: 'dashed', func:'#388f58' } },
+
+        { data: { source: 'k', target: 'j' , func:'#39b3d7'}  },
+        { data: { source: 'k', target: 'e', type: 'dashed', func: '#dc2c0f' } },
+        { data: { source: 'k', target: 'g', type: 'dashed', func: '#dc2c0f' } },
+
+        { data: { source: 'g', target: 'j' , func:'#39b3d7' } }
+      ],
+    },
+
+    ready: function(){
+      cy = this;
+    }
+  };
+
+  $('#cy').cytoscape(options);
+
+});
+	</script>
+	</body>
+
+</html>
